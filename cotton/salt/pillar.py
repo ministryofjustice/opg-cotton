e2e29172e3ad010a33367f8db4cd1c5f14f62992
Brett Minnie <github@bdb-studios.co.uk>
2016-05-09 13:42:12 +0100

    Cleanup refactor

--- cotton/salt/pillar.py
+++ cotton/salt/pillar.py
@@ -47,7 +47,6 @@ def _get_projects_location():
 
 
 def get_rendered_pillar_location(pillar_dir=None, projects_location=None, parse_top_sls=True):
-
     """
     Returns path to rendered pillar.
     Use to render pillars written in jinja locally not to upload unwanted data to network.
@@ -63,10 +62,6 @@ def get_rendered_pillar_location(pillar_dir=None, projects_location=None, parse_
 
     In case there is no top.sls in pillar root than it returns: None
     """
-    from jinja2 import Environment
-    from jinja2 import FileSystemLoader
-    from jinja2.exceptions import TemplateNotFound
-
     if 'use_project_dir' not in 'env':
         env.use_project_dir = False
 
@@ -74,6 +69,66 @@ def get_rendered_pillar_location(pillar_dir=None, projects_location=None, parse_
         projects_location = _get_projects_location()
 
     pillars = __load_pillar_dirs(pillar_dir, projects_location)
+    jinja_env = _set_template_env(pillars, projects_location)
+
+    dest_location = tempfile.mkdtemp()
+
+    if parse_top_sls:
+        files_to_render = _pillar_from_top_sls(dest_location, jinja_env)
+    elif len(pillars):
+        files_to_render = _pillar_from_dirs(pillars)
+    else:
+        print(yellow("No template files where found to render"))
+        files_to_render = []
+
+    if __render_templates(files_to_render, dest_location, jinja_env) is False:
+        print(red("Aborting due to pillar failing to render"))
+        exit(-1)
+
+    return dest_location
+
+
+def _pillar_from_dirs(pillars):
+    files_to_render = []
+    # let's select all files from pillar directory
+    for pillar in pillars:
+        for root, dirs, files in os.walk(pillar):
+            rel_path = os.path.relpath(root, pillar)
+            for file_name in files:
+                files_to_render.append(os.path.join(rel_path, file_name))
+
+    return files_to_render
+
+
+def _pillar_from_top_sls(dest_location, jinja_env):
+    from jinja2.exceptions import TemplateNotFound
+
+    files_to_render = []
+
+    # let's parse top.sls to only select files being referred in top.sls
+    try:
+        top_sls = jinja_env.get_template('top.sls').render(env=env)
+    except TemplateNotFound:
+        raise RuntimeError("Missing top.sls in pillar location. Skipping rendering.")
+    top_content = yaml.load(top_sls)
+    filename = os.path.join(dest_location, 'top.sls')
+    with open(filename, 'w') as f:
+        print("Pillar template_file: {} --> {}".format('top.sls', filename))
+        f.write(top_sls)
+    for k0, v0 in top_content.iteritems():
+        for k1, v1 in v0.iteritems():
+            for file_short in v1:
+                # We force this file to be relative in case jinja failed rendering
+                # a variable. This would make the filename start with / and instead of
+                # writing under dest_location it will try to write in /
+                if isinstance(file_short, str):
+                    files_to_render.append('./' + file_short.replace('.', '/') + '.sls')
+
+    return files_to_render
+
+def _set_template_env(pillars, projects_location):
+    from jinja2 import Environment
+    from jinja2 import FileSystemLoader
 
     # We need to merge our directory trees here so that we don't mess with the pillars list
     # if so we may try to connect to something that doesn't actually exist
@@ -81,50 +136,11 @@ def get_rendered_pillar_location(pillar_dir=None, projects_location=None, parse_
     template_dirs.append(projects_location)
     # String out None or anything empty
     template_dirs = filter(None, template_dirs)
-
     if len(template_dirs):
         jinja_env = Environment(loader=FileSystemLoader(template_dirs))
     else:
         raise RuntimeError("No source template directories are specified, aborting")
-
-    files_to_render = []
-    dest_location = tempfile.mkdtemp()
-
-    if parse_top_sls:
-        # let's parse top.sls to only select files being referred in top.sls
-        try:
-            top_sls = jinja_env.get_template('top.sls').render(env=env)
-        except TemplateNotFound:
-            raise RuntimeError("Missing top.sls in pillar location. Skipping rendering.")
-
-        top_content = yaml.load(top_sls)
-
-        filename = os.path.join(dest_location, 'top.sls')
-        with open(filename, 'w') as f:
-            print("Pillar template_file: {} --> {}".format('top.sls', filename))
-            f.write(top_sls)
-
-        for k0, v0 in top_content.iteritems():
-            for k1, v1 in v0.iteritems():
-                for file_short in v1:
-                    # We force this file to be relative in case jinja failed rendering
-                    # a variable. This would make the filename start with / and instead of
-                    # writing under dest_location it will try to write in /
-                    if isinstance(file_short, str):
-                        files_to_render.append('./' + file_short.replace('.', '/') + '.sls')
-    elif len(pillars):
-        # let's select all files from pillar directory
-        for pillar in pillars:
-            for root, dirs, files in os.walk(pillar):
-                rel_path = os.path.relpath(root, pillar)
-                for file_name in files:
-                    files_to_render.append(os.path.join(rel_path, file_name))
-
-    if __render_templates(files_to_render, dest_location, jinja_env) is False:
-        print(red("Aborting due to pillar failing to render"))
-        exit(-1)
-
-    return dest_location
+    return jinja_env
 
 
 def __load_pillar_dirs(pillar_dir, projects_location):
