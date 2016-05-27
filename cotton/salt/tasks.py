b9ba8c28f049c8cee16a83968fb68a439ebbf63a
Brett Minnie <github@bdb-studios.co.uk>
2016-05-27 12:49:58 +0100

    Enable psuedo-tty so we can capture output

--- cotton/salt/tasks.py
+++ cotton/salt/tasks.py
@@ -31,7 +31,7 @@ def unattended_highstate():
 
 @vm_task
 def highstate_complete():
-    result = salt_run(method='jobs.active', quiet=True, stdout=True)
+    result = salt_run(method='jobs.active', quiet=True, pty=True)
 
     print result
     if result is not None:
