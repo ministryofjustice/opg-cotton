9c9b1732eee9fedf00736e0355fa636a17c25347
Brett Minnie <github@bdb-studios.co.uk>
2016-06-10 12:13:27 +0100

    Bumped version and added actual commit

--- cotton/gitutils/gitutilities.py
+++ cotton/gitutils/gitutilities.py
@@ -1,6 +1,6 @@
 import os
 from git import Repo
-from git.exc import GitCommandError
+from cotton.colors import yellow
 
 
 class GitUtilities(object):
@@ -20,20 +20,32 @@ class GitUtilities(object):
     def commit_change_set(self):
         self._stash_changes()
         self._checkout_branch()
-        self._pop_changes()
         self._pull_branch()
+        self._pop_changes()
+        self._git_commit(self.change_set, self.message)
+
+    def _git_commit(self, changes=[], message=''):
+        print(yellow("Staging change-set"))
+        for change in changes:
+            self.git.git.add(change)
+        print(yellow("Committing files with message {}".format(message)))
+        self.git.git.commit('-a -m "{}"'.format(message))
 
     def _git_status(self):
         return self.git.git.status()
 
     def _stash_changes(self):
+        print(yellow("Stashing changes"))
         self.git.git.stash('save')
 
     def _pop_changes(self):
+        print(yellow("Popping changes"))
         self.git.git.stash('pop')
 
     def _checkout_branch(self, branch_name='master'):
+        print(yellow("Checking out {}".format(branch_name)))
         self.git.git.checkout(branch_name)
 
     def _pull_branch(self):
+        print(yellow("Rebasing against branch"))
         self.git.git.pull('--rebase')
